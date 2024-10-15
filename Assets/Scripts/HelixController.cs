dae64f1a45649dd365f77fa7d33e38c90ef7aee2
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-10-15 13:48:07 +0300

    Add helix Movement
    - add helix controller script to move the helix left and right

---
+++ Assets/Scripts/HelixController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixController : MonoBehaviour
+{
+    //know last screen tap
+    private Vector2 lastTapPosition;
+    private Vector3 startRoatation;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startRoatation = transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for touchscreen
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 currentTapPosition = Input.mousePosition;
+
+            if(lastTapPosition == Vector2.zero)
+            {
+                lastTapPosition = currentTapPosition;
+            }
+
+            float delta = lastTapPosition.x - currentTapPosition.x;
+            lastTapPosition = currentTapPosition;
+
+            transform.Rotate(Vector3.up * delta);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            lastTapPosition = Vector2.zero;
+        }
+    }
+}
